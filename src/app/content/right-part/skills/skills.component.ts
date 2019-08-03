d2ed3fe9fa89f27f785484dfc4373f4abaeaddd4
Henry Leon Gomez <helg18@gmail.com>
2019-08-03 01:33:46 -0400

    Adding animations to Skills components

--- src/app/content/right-part/skills/skills.component.ts
+++ src/app/content/right-part/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from "./../../../../assets/js/jquery";
 
 @Component({
   selector: 'app-skills',
@@ -10,6 +11,17 @@ export class SkillsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function () {
+      $('.arlo_tm_progress').each(function(i) {
+        var progress 		= $(this);
+        var pValue 			= parseInt(progress.data('value'), 10);
+        var pColor			= progress.data('color');
+        var pBarWrap 		= progress.find('.arlo_tm_bar_wrap');
+        var pBar 			= progress.find('.arlo_tm_bar');
+        pBar.css({ width: pValue+'%', backgroundColor: pColor });
+        setTimeout(function(){pBarWrap.addClass('open');},(i*500));
+      });
+    });
   }
 
 }
