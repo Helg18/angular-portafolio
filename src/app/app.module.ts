d4144dfca01523174a7ef11824ba674fc04a00d7
Henry Leon Gomez <helg18@gmail.com>
2019-08-02 23:53:27 -0400

    Moved All Components From Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
+import { ContentComponent } from './content/content.component';
+import { LeftPartComponent } from './content/left-part/left-part.component';
+import { RightPartComponent } from './content/right-part/right-part.component';
+import { AboutComponent } from './content/right-part/about/about.component';
+import { SkillsComponent } from './content/right-part/skills/skills.component';
+import { ServicesComponent } from './content/right-part/services/services.component';
+import { PortfolioFilterComponent } from './content/right-part/portfolio/portfolio-filter/portfolio-filter.component';
+import { PortfolioComponent } from './content/right-part/portfolio/portfolio.component';
+import { TestimonialsComponent } from './content/right-part/testimonials/testimonials.component';
+import { CounterBoxComponent } from './content/right-part/counter-box/counter-box.component';
+import { NewsComponent } from './content/right-part/news/news.component';
+import { FooterComponent } from './content/right-part/contact-footer/footer/footer.component';
+import { ContactFooterComponent } from './content/right-part/contact-footer/contact-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MobileMenuComponent,
+    ContentComponent,
+    LeftPartComponent,
+    RightPartComponent,
+    AboutComponent,
+    SkillsComponent,
+    ServicesComponent,
+    PortfolioFilterComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    CounterBoxComponent,
+    NewsComponent,
+    FooterComponent,
+    ContactFooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/content/right-part/counter-box/counter-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-box',
+  templateUrl: './counter-box.component.html',
+  styleUrls: ['./counter-box.component.css']
+})
+export class CounterBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
